7a225a94a0aa19ae6193e23fdbf26c90a2e08fe5
Jeferson Luis <jgjefersonluis@gmail.com>
2021-04-02 18:00:01 -0300

    projeto pronto para o deploy

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-# from .views import IndexView
-from core.views import IndexView
+from .views import IndexView
 
 urlpatterns = [
-        path('', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
 ]
+
